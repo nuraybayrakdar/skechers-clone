1fe9c2291c5878a0ca788b13a9a4e5ee2c0c9a3d
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-12-07 19:43:34 +0300

    slider container denemesi

---
+++ components/SliderContainer.vue
@@ -0,0 +1,451 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+  />
+  <div class="cl-slider-container">
+    <p class="cl-slider-header">Sizin İçin Seçtiklerimiz</p>
+    <div class="cl-slider-tab-buttons" style="display: none"></div>
+    <div
+      class="swiper-container cl-product-slider enable-other-colors swiper-container-initialized swiper-container-horizontal active"
+      data-recommender-code="690ef8daccd5da77d89736c364ab94b9"
+    >
+      <div
+        class="swiper-wrapper"
+        id="swiper-wrapper-f7af5cfa3aaea9b6"
+        aria-live="polite"
+        style="
+          transform: translate3d(0px, 0px, 0px);
+          transition: all 0ms ease 0s;
+        "
+      >
+        <div
+          id="149729-BBK"
+          class="sk-rec-product swiper-slide"
+          role="group"
+          aria-label="6 / 12"
+          style="width: 309px; margin-right: 15px"
+        >
+          <a
+            class="cl-product-images"
+            title=""
+            href="https://www.skechers.com.tr/p-arch-fit-kadin-siyah-spor-ayakkabi-149729-bbk?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+            target="_self"
+          >
+            <img
+              class="lazy"
+              loading="lazy"
+              src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/149729%20BBK/1.jpg"
+              data-src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/149729%20BBK/1.jpg"
+              title=""
+              alt=""
+            />
+            <div class="slid-color">
+              <a
+                href="/p-arch-fit-kadin-gri-spor-ayakkabi-149729-char?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20CHAR/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20CHAR/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/149729 CHAR/1.jpg"
+              ></a
+              ><a
+                href="/p-arch-fit-kadin-siyah-spor-ayakkabi-149729-bbk?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20BBK/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20BBK/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/149729 BBK/1.jpg"
+              ></a>
+            </div>
+          </a>
+          <div>
+            <div class="cl-product-info">
+              <a
+                title=""
+                href="https://www.skechers.com.tr/p-arch-fit-kadin-siyah-spor-ayakkabi-149729-bbk?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                target="_self"
+              >
+                <span class="cl-product-name">ARCH FİT </span><br />
+                <span class="cl-product-category"
+                  >Kadın Siyah Spor Ayakkabı</span
+                >
+              </a>
+            </div>
+            <div class="cl-flex-row">
+              <div class="cl-product-prices">
+                <div class="cl-product-old-price">
+                  <span>1949 ₺</span
+                  ><span class="cl-product-discount-rate">-%35</span>
+                </div>
+                <span> 1266 ₺</span>
+              </div>
+              <div class="total-colors-count"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          id="149729-CHAR"
+          class="sk-rec-product swiper-slide"
+          role="group"
+          aria-label="7 / 12"
+          style="width: 309px; margin-right: 15px"
+        >
+          <a
+            class="cl-product-images"
+            title=""
+            href="https://www.skechers.com.tr/p-arch-fit-kadin-gri-spor-ayakkabi-149729-char?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+            target="_self"
+          >
+            <img
+              class="lazy"
+              loading="lazy"
+              src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/149729%20CHAR/1.jpg"
+              data-src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/149729%20CHAR/1.jpg"
+              title=""
+              alt=""
+            />
+            <div class="slid-color">
+              <a
+                href="/p-arch-fit-kadin-gri-spor-ayakkabi-149729-char?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20CHAR/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20CHAR/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/149729 CHAR/1.jpg"
+              ></a
+              ><a
+                href="/p-arch-fit-kadin-siyah-spor-ayakkabi-149729-bbk?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20BBK/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/149729%20BBK/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/149729 BBK/1.jpg"
+              ></a>
+            </div>
+          </a>
+          <div>
+            <div class="cl-product-info">
+              <a
+                title=""
+                href="https://www.skechers.com.tr/p-arch-fit-kadin-gri-spor-ayakkabi-149729-char?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                target="_self"
+              >
+                <span class="cl-product-name">ARCH FİT </span><br />
+                <span class="cl-product-category">Kadın Gri Spor Ayakkabı</span>
+              </a>
+            </div>
+            <div class="cl-flex-row">
+              <div class="cl-product-prices">
+                <div class="cl-product-old-price">
+                  <span>1949 ₺</span
+                  ><span class="cl-product-discount-rate">-%25</span>
+                </div>
+                <span> 1461 ₺</span>
+              </div>
+              <div class="total-colors-count"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          id="232330-BKGY"
+          class="sk-rec-product swiper-slide"
+          role="group"
+          aria-label="8 / 12"
+          style="width: 309px; margin-right: 15px"
+        >
+          <a
+            class="cl-product-images"
+            title=""
+            href="https://www.skechers.com.tr/p-glide-step-erkek-siyah-spor-ayakkabi-232330-bkgy?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+            target="_self"
+          >
+            <img
+              class="lazy"
+              loading="lazy"
+              src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/232330%20BKGY/1.jpg"
+              data-src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/232330%20BKGY/1.jpg"
+              title=""
+              alt=""
+            />
+            <div class="slid-color">
+              <a
+                href="/p-glide-step-erkek-bej-spor-ayakkabi-232330-tpnv?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/232330%20TPNV/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/232330%20TPNV/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/232330 TPNV/1.jpg"
+              ></a
+              ><a
+                href="/p-glide-step-erkek-siyah-spor-ayakkabi-232330-bkgy?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/232330%20BKGY/1.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/232330%20BKGY/1.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/232330 BKGY/1.jpg"
+              ></a>
+            </div>
+          </a>
+          <div>
+            <div class="cl-product-info">
+              <a
+                title=""
+                href="https://www.skechers.com.tr/p-glide-step-erkek-siyah-spor-ayakkabi-232330-bkgy?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                target="_self"
+              >
+                <span class="cl-product-name">GLİDE-STEP </span><br />
+                <span class="cl-product-category"
+                  >Erkek Siyah Spor Ayakkabı</span
+                >
+              </a>
+            </div>
+            <div class="cl-flex-row">
+              <div class="cl-product-prices">
+                <div class="cl-product-old-price">
+                  <span>1849 ₺</span
+                  ><span class="cl-product-discount-rate">-%25</span>
+                </div>
+                <span> 1386 ₺</span>
+              </div>
+              <div class="total-colors-count"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          id="S222521-001"
+          class="sk-rec-product swiper-slide"
+          role="group"
+          aria-label="12 / 12"
+          style="width: 309px; margin-right: 15px"
+        >
+          <a
+            class="cl-product-images"
+            title=""
+            href="https://www.skechers.com.tr/p-w-lw-two-yarn-slim-sweatpant-kadin-siyah-esofman-alti-s222521-001?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+            target="_self"
+          >
+            <img
+              class="lazy"
+              loading="lazy"
+              src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/S222521-001/S_0260.jpg"
+              data-src="https://skcfiles.mncdn.com/mnresize/500/500/livephotos/8/S222521-001/S_0260.jpg"
+              title=""
+              alt=""
+            />
+            <div class="slid-color">
+              <a
+                href="/p-w-lw-two-yarn-slim-sweatpant-kadin-beyaz-esofman-alti-s222521-102?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-102/01.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-102/01.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/S222521-102/01.jpg"
+              ></a
+              ><a
+                href="/p-w-lw-two-yarn-slim-sweatpant-kadin-siyah-esofman-alti-s222521-001?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-001/S_0260.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-001/S_0260.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/S222521-001/S_0260.jpg"
+              ></a
+              ><a
+                href="/p-w-lw-two-yarn-slim-sweatpant-kadin-gri-esofman-alti-s222521-036?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                class="pColor lazy"
+                data-src="https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-036/01.jpg"
+                style="
+                  background-image: url('https://skcfiles.mncdn.com/mnresize/80/80/livephotos/8/S222521-036/01.jpg');
+                "
+                data-img="https://skcfiles.mncdn.com/mnresize/600/600/livephotos/8/S222521-036/01.jpg"
+              ></a>
+            </div>
+          </a>
+          <div>
+            <div class="cl-product-info">
+              <a
+                title=""
+                href="https://www.skechers.com.tr/p-w-lw-two-yarn-slim-sweatpant-kadin-siyah-esofman-alti-s222521-001?recommended_by=dynamic&amp;recommended_code=690ef8daccd5da77d89736c364ab94b9"
+                target="_self"
+              >
+                <span class="cl-product-name"
+                  >W LW Two Yarn Slim Sweatpant </span
+                ><br />
+                <span class="cl-product-category"
+                  >Kadın Siyah Eşofman Altı</span
+                >
+              </a>
+            </div>
+            <div class="cl-flex-row">
+              <div class="cl-product-prices">
+                <div class="cl-product-old-price">
+                  <span>999 ₺</span
+                  ><span class="cl-product-discount-rate">-%25</span>
+                </div>
+                <span> 749 ₺</span>
+              </div>
+              <div class="total-colors-count"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="swiper-button-next"
+        tabindex="0"
+        role="button"
+        aria-label="Next slide"
+        aria-controls="swiper-wrapper-f7af5cfa3aaea9b6"
+        aria-disabled="false"
+      ></div>
+      <div
+        class="swiper-button-prev swiper-button-disabled"
+        tabindex="-1"
+        role="button"
+        aria-label="Previous slide"
+        aria-controls="swiper-wrapper-f7af5cfa3aaea9b6"
+        aria-disabled="true"
+      ></div>
+      <span
+        class="swiper-notification"
+        aria-live="assertive"
+        aria-atomic="true"
+      ></span>
+    </div>
+  </div>
+</template>
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+};
+</script>
+<style>
+.cl-slider-container {
+  background-color: #f4f5f6;
+  margin: 0px;
+  padding: 40px 0;
+}
+
+.cl-slider-container
+  .cl-slider-tab-buttons-container
+  .cl-product-slider-header {
+  font-size: 18px;
+  font-weight: 800;
+  color: var(--blue);
+  line-height: 42px;
+  padding: 0 20px 0 100px;
+}
+
+.cl-slider-container
+  .cl-slider-tab-buttons-container
+  .cl-product-slider-header
+  .cl-slider-tab-buttons {
+  margin-left: 0px;
+  margin-right: auto;
+}
+
+.cl-slider-container .cl-slider-tab-buttons-container {
+  display: flex;
+}
+.cl-product-slider {
+  min-height: 580px;
+}
+@media only screen and (max-width: 767px) {
+  .cl-slider-header {
+    color: #000;
+    font-size: 18px;
+    font-weight: 600;
+  }
+  .cl-slider-container {
+    margin: 0;
+  }
+  .cl-slider-container .cl-slider-tab-buttons {
+    width: calc(100vw - 30px) !important;
+  }
+  .cl-slider-container .cl-slider-tab-buttons {
+    justify-content: flex-start;
+  }
+}
+
+@media only screen and (max-width: 1920px) {
+  .cl-product-slider {
+    min-height: calc(31.25vw + 10px) !important;
+  }
+}
+@media only screen and (max-width: 991px) {
+  .cl-slider-container .cl-slider-tab-buttons-container {
+    display: block;
+  }
+
+  .cl-slider-container
+    .cl-slider-tab-buttons-container
+    .cl-product-slider-header {
+    padding: 0 12px;
+    text-align: center;
+    font-size: 14px;
+    line-height: 20px;
+    margin-bottom: 16px;
+  }
+  .cl-slider-container
+    .cl-slider-tab-buttons-container
+    .cl-product-slider-header
+    .cl-slider-tab-buttons {
+    font-size: 14px;
+    padding: 0 10px;
+    line-height: 34px;
+  }
+  .cl-product-slider {
+    min-height: calc(103vw + 10px) !important;
+  }
+}
+#cl-main-slider {
+  overflow: unset !important;
+}
+.sk-rec-product swiper-slide{
+   margin-right: 15px;
+}
+.swiper {
+    margin-left: auto;
+    margin-right: auto;
+    position: relative;
+    overflow: hidden;
+    list-style: none;
+    padding: 0;
+    /* Fix of Webkit flickering */
+    z-index: 1;
+  }
+  .swiper-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    transition-property: transform;
+    box-sizing: content-box;
+  }
+  .swiper-android .swiper-slide,
+.swiper-wrapper {
+  transform: translate3d(0px, 0, 0);
+}
+.swiper-slide {
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition-property: transform;
+}
+.swiper-slide-invisible-blank {
+  visibility: hidden;
+}
+</style>
