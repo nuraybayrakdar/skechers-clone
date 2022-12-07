3eda4fc119c4e566a2ca0bfdcc59aefdda9c203f
nuray <83471802+nuraybayrakdar@users.noreply.github.com>
2022-12-08 00:50:35 +0300

    app config delete

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/oketh/OneDrive/Belgeler/GitHub/skechers-clone/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
