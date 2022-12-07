55b44608719b68f3982aa75f86a696f336ce664c
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-12-07 04:31:24 +0300

    discover banner add

---
+++ components/DiscoverBanner.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="banner">
+    <div id="band-banner" class="cl-ad-banner cl-ad-banner-full-width">
+      <a
+        href="/web-ozel-urunler-c-8028"
+        class="cl-prom-item viewed"
+        data-id="tag1635197822386"
+        data-name="Sadece Online'da"
+        data-creative="Sadece Online'da"
+        data-position="0"
+        alt="Sadece Online'da"
+      >
+        <picture>
+          <source
+            media="(max-width:991px)"
+            srcset="
+              https://skcfiles.mncdn.com/webfiles/banners/638054822624874381-webe-ozel-bant-mobile.jpg?v=1669908376
+            "
+          />
+          <img
+            class=""
+            src="https://skcfiles.mncdn.com/webfiles/banners/638054822587728564-webe-ozel-site-bant.jpg?v=1669908376"
+            data-src="https://skcfiles.mncdn.com/webfiles/banners/638054822587728564-webe-ozel-site-bant.jpg?v=1669908376"
+          />
+        </picture>
+      </a>
+    </div>
+    <div
+      class="cl-html-banner-2 cl-prom-item"
+      data-id="tag1635197960139"
+      data-name="Stilinin Tamamlayıcısı: Stamina"
+      data-creative="Stilinin Tamamlayıcısı: Stamina"
+      data-position="0"
+      alt="Stilinin Tamamlayıcısı: Stamina"
+    >
+      <img
+        src="https://skcfiles.mncdn.com/webfiles/banners/638037598374780814-c1-stamina-dektop.jpg?v=1668163064"
+        data-src="https://skcfiles.mncdn.com/webfiles/banners/638037598374780814-c1-stamina-dektop.jpg?v=1668163064"
+        class="cl-html-banner-2-img"
+        data-mobsrc="https://skcfiles.mncdn.com/webfiles/banners/638037598415071777-c1-stamina-mobile.jpg?v=1668163064"
+      />
+      <p class="cl-html-banner-2-subtitle"></p>
+      <p class="cl-html-banner-2-title">Stilinin Tamamlayıcısı: Stamina</p>
+      <a href="/stamina-c-4015" class="cl-html-banner-2-button">Hemen Keşfet</a>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+
+.banner{
+    font-family: Mulish, sans-serif;
+}
+
+#band-banner.cl-ad-banner {
+  margin: 0;
+}
+
+#band-banner.cl-ad-banner.cl-ad-banner-full-width {
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+#band-banner.cl-ad-banner img {
+  width: 100%;
+  aspect-ratio: 1920/180;
+  background-color: #f9f6f6;
+}
+
+#band-banner.cl-ad-banner .cl-mobile-img {
+  display: none;
+}
+
+@media only screen and (max-width: 991px) {
+  #band-banner.cl-ad-banner {
+    margin: 0%;
+  }
+
+  #band-banner.cl-ad-banner.cl-ad-banner-full-width {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  #band-banner.cl-ad-banner .cl-desktop-img {
+    display: none;
+  }
+
+  #band-banner.cl-ad-banner .cl-mobile-img {
+    display: block;
+  }
+
+  #band-banner.cl-ad-banner img {
+    aspect-ratio: 768/220;
+  }
+}
+
+.cl-html-banner-2 {
+  margin-bottom: 80px;
+  text-align: center;
+  color: #022c57;
+}
+
+.cl-html-banner-2-img {
+  width: 100%;
+  aspect-ratio: 1920/730;
+  background-color: #eee;
+}
+
+.cl-html-banner-2-title {
+  font-size: 50px;
+  font-weight: 900;
+  margin: 0% 15px 30px;
+}
+
+.cl-html-banner-2-subtitle {
+  font-size: 20px;
+  font-weight: 400;
+  margin: 30px 15px 10px;
+}
+
+.cl-html-banner-2-button {
+  font-size: 16px;
+  font-weight: 900;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 46px;
+  border: 1px solid #022c57;
+  background-color: #022c57;
+  color: #fff;
+  transition: background-color 0.3s, color 0.3s;
+  border-radius: 6px;
+  padding: 0 56px;
+  width: fit-content;
+  margin: auto;
+}
+
+.cl-html-banner-2-button:hover {
+  color: #022c57;
+  background-color: transparent;
+}
+
+.cl-html-banner-2-img.cl-mobile-img {
+  display: none;
+}
+
+@media only screen and (max-width: 991px) {
+  .cl-html-banner-2 {
+    text-align: left;
+    margin-bottom: 50px;
+  }
+
+  .cl-html-banner-2-title {
+    font-size: 24px;
+    margin: 0% 15px 16px;
+  }
+
+  .cl-html-banner-2-subtitle {
+    font-size: 12px;
+    margin: 10px 15px 6px;
+  }
+
+  .cl-html-banner-2-button {
+    margin: 0% 15px;
+    font-size: 12px;
+    font-weight: 400;
+    height: 16px;
+    border: none;
+    background-color: transparent;
+    color: #022c57;
+    padding: 0%;
+    width: fit-content;
+    text-decoration: underline;
+  }
+
+  .cl-html-banner-2-img.cl-mobile-img {
+    display: block;
+  }
+
+  .cl-html-banner-2-img.cl-desktop-img {
+    display: none;
+  }
+
+  .cl-html-banner-2-img {
+    aspect-ratio: 1920/730;
+  }
+}
+</style>
