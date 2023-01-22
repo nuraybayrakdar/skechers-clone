082829dec99999a79dfdea66187e32cd0d079599
nuraybayrakdar <bayrakdarnuray@gmail.com>
2023-01-22 12:40:53 +0300

    Revert "firebase" This reverts commit 7dff59a5b37dd4a33cc9c66e5aaf5accc2c85840.

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/bayra/Documents/Github/skechers-clone/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- main.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueFire from 'vuefire'
-
-Vue.use(VueFire)
