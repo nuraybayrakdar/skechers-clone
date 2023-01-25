f310ddbf57db57a5fe0e1db1d7ee14d7b53c8552
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-01-25 22:56:17 +0300

    changes

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <ProductCard
-        v-bind:image="'src/assets/sh1.png'"
-        name=" Uno - Crossing Hearts 155227 WMLT"
-        desc=" Beyaz Sneakers"
-        color="Beyaz"
-        price="1429.99 TL"
-      ></ProductCard>
-    </div>
+  <div>
+    <ProductCard
+      v-bind:image="'src/assets/sh1.png'"
+      name=" Uno - Crossing Hearts 155227 WMLT"
+      desc=" Beyaz Sneakers"
+      color="Beyaz"
+      price="1429.99 TL"
+    ></ProductCard>
+  </div>
 </template>
 <script setup lang="ts">
 import ProductCard from "@/components/ProductCard.vue";
@@ -15,18 +15,18 @@ import ProductCard from "@/components/ProductCard.vue";
 
 <style>
 body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-    background-color: #f7f8fc;
-    font-family: "Roboto", sans-serif;
-    color: #10182f;
-  }
-  .container2 {
-    display: flex;
-    width: 1040px;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  background-color: #f7f8fc;
+  font-family: "Roboto", sans-serif;
+  color: #10182f;
+}
+.container2 {
+  display: flex;
+  width: 1040px;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
 </style>
