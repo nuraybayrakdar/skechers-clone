ca394a2cb0760773643f9a00347b02e7e33c4725
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-01-26 16:36:07 +0300

    add pinia

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,24 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: () => ({
+    count: 0,
+    /*
+    image: "'src/assets/sh1.png'",
+    name: " Uno - Crossing Hearts 155227 WMLT",
+    desc: " Beyaz Sneakers",
+    color: "Beyaz",
+    price: "1429.99 TL",
+    */
+  }),
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+  },
+});
